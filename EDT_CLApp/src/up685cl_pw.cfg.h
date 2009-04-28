66aec28aa2af6fa5efc9b0793b26259adee74e6c
no author <noauthor@slac.stanford.edu>
2009-04-28 05:44:50 +0000

    This commit was manufactured by cvs2svn to create tag 'EDT_CL-R1-4-0'.
    Sprout from master 2009-04-28 05:30:28 UTC Sheng Peng <pengs@slac.stanford.edu> 'Add UP685 support, both 10b and 8b, both freerun and pulse width mode'
    Cherrypick from master 2009-04-18 02:08:13 UTC Sheng Peng <pengs@slac.stanford.edu> 'Add UP685 support':
        EDT_CLApp/Db/camera660490.db
        EDT_CLApp/src/devUP685CL.c
        EDT_CLApp/src/up685cl.cfg.h
        EDT_CLApp/src/up685cl_pw.cfg.h
    Cherrypick from master 2009-04-28 05:44:49 UTC Sheng Peng <pengs@slac.stanford.edu> 'Remove camera660490.db':
        EDT_CLApp/Db/Makefile

---
+++ EDT_CLApp/src/up685cl_pw.cfg.h
@@ -0,0 +1,19 @@
+{
+    "camera_class: \"Uniq\"",
+    "camera_model: \"UP685CL\"",
+    "camera_info: \"658 x 491,10bits, free run\"",
+    "width: 692",
+    "height: 501",
+    "depth: 10",
+    "extdepth: 10",
+    "rbtfile: aiagcl.bit",
+    "MODE_CNTL_NORM: b0",
+    "CL_DATA_PATH_NORM: 09",
+    "CL_CFG_NORM: 00",
+    "hskip: 18",
+    "hactv: 660",
+    "vskip: 7",
+    "vactv: 490",
+    ""
+};
+
