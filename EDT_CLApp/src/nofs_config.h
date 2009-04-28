1288975a79668487185d1ea7fdf4b3033d1a6685
Sheng Peng <pengs@slac.stanford.edu>
2009-04-28 05:23:58 +0000

    remove up685 cfg, add up685 8b/10b cfg

--- EDT_CLApp/src/nofs_config.h
+++ EDT_CLApp/src/nofs_config.h
@@ -13,11 +13,17 @@ char *up900cl12b_cfg[] =
 char *ptm4200cl_pw_cfg[] =
 #include "ptm4200cl_pw.cfg.h"
 
-char *up685cl_cfg[] =
-#include "up685cl.cfg.h"
+char *up685cl8b_cfg[] =
+#include "up685cl8b.cfg.h"
 
-char *up685cl_pw_cfg[] =
-#include "up685cl_pw.cfg.h"
+char *up685cl8b_pw_cfg[] =
+#include "up685cl8b_pw.cfg.h"
+
+char *up685cl10b_cfg[] =
+#include "up685cl10b.cfg.h"
+
+char *up685cl10b_pw_cfg[] =
+#include "up685cl10b_pw.cfg.h"
 
 #define MAPCONFIG(cf, vx_p) \
      if (strcmp(cf, "ptm6710cl") == 0) \
@@ -28,10 +34,14 @@ char *up685cl_pw_cfg[] =
         vx_p = up900cl12b_cfg ; \
      else if (strcmp(cf, "ptm4200cl_pw") == 0) \
         vx_p = ptm4200cl_pw_cfg ; \
-     else if (strcmp(cf, "up685cl") == 0) \
-        vx_p = up685cl_cfg ; \
-     else if (strcmp(cf, "up685cl_pw") == 0) \
-        vx_p = up685cl_pw_cfg ; \
+     else if (strcmp(cf, "up685cl8b") == 0) \
+        vx_p = up685cl8b_cfg ; \
+     else if (strcmp(cf, "up685cl8b_pw") == 0) \
+        vx_p = up685cl8b_pw_cfg ; \
+     else if (strcmp(cf, "up685cl10b") == 0) \
+        vx_p = up685cl10b_cfg ; \
+     else if (strcmp(cf, "up685cl10b_pw") == 0) \
+        vx_p = up685cl10b_pw_cfg ; \
      else vx_p = NULL ;
 
 #endif /* _nofs_config_h_ */
--- EDT_CLApp/src/up685cl10b.cfg.h
+++ EDT_CLApp/src/up685cl10b.cfg.h
@@ -3,17 +3,17 @@
     "camera_model: \"UP685CL\"",
     "camera_info: \"658 x 491,10bits, free run\"",
     "width: 692",
-    "height: 501",
+    "height: 490",
     "depth: 10",
     "extdepth: 10",
     "rbtfile: aiagcl.bit",
-    "MODE_CNTL_NORM: b0",
+    "MODE_CNTL_NORM:    00",
     "CL_DATA_PATH_NORM: 09",
-    "CL_CFG_NORM: 00",
-    "hskip: 18",
-    "hactv: 660",
-    "vskip: 7",
-    "vactv: 490",
+    "CL_CFG_NORM: 02",
+    "hskip: 20",
+    "hactv: 640",
+    "vskip: 6",	/* give one row for splitted image detection */
+    "vactv: 480",
     ""
 };
 
